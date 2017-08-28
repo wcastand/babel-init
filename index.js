791ea6c961af439b0b5444b0c7d5c235f5141cc2
wcastand <wcastand@gmail.com>
2017-08-28 19:13:03 +0200

    change final msg

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-const yeoman = require('yeoman-environment');
-const env = yeoman.createEnv();
-const babelInit = require('./init');
+const yeoman = require('yeoman-environment')
+const env = yeoman.createEnv()
+const babelInit = require('./init')
 
-env.registerStub(babelInit, 'babel:init');
-env.run('babel:init', () => console.log('done'));
+env.registerStub(babelInit, 'babel:init')
+env.run('babel:init', () =>
+  console.log('.babelrc created and dependencies added.')
+)
